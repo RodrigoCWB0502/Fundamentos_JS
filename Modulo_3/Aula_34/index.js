0b825fafd5e799c06a944318be1a9a0ceedd0bf7
Rodrigo <rodrigogb0502@gmail.com>
2025-07-15 11:23:51 -0300

    Aula 34 | Parte 1/2

---
+++ Modulo_3/Aula_34/index.js
@@ -0,0 +1,48 @@
+function addContact() {
+  const contactSection = document.getElementById('contacts-list');
+
+  const h3 = document.createElement('h3');
+  h3.innerText = 'Contato';
+
+  const ul = document.createElement('ul');
+
+  const nameLi = document.createElement('li');
+  nameLi.innerText = 'Nome';
+
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.name = 'fullname';
+
+  nameLi.appendChild(nameInput);
+
+  ul.appendChild(nameLi);
+
+  const phoneLi = document.createElement('li');
+  phoneLi.innerText = 'Telefone';
+
+  const phoneInput = document.createElement('input');
+  phoneInput.type = 'text';
+  phoneInput.name = 'phone';
+
+  phoneLi.appendChild(phoneInput);
+
+  ul.appendChild(phoneLi);
+
+  const addressLi = document.createElement('li');
+  addressLi.innerHTML = '<label for="address">Endereço: </label>';
+
+  const addressInput = document.createElement('input');
+  addressInput.type = 'text';
+  phoneInput.name = 'Address';
+
+  addressLi.appendChild(addressInput);
+
+  ul.appendChild(addressLi);
+
+  const saveContact = document.createElement('button');
+  saveContact.innerText = 'Salvar contato';
+
+  ul.appendChild(saveContact);
+
+  contactSection.append(h3, ul);
+}
